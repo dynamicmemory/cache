28e7eb4b2e3e476404ba9a88710141b187a7d3a6
dynamicmemory <joshua.hahn@live.com>
2025-12-12 17:46:32 +1000

    Added column editing (again)

--- src/ViewModels/ColumnViewModel.cs
+++ src/ViewModels/ColumnViewModel.cs
@@ -13,12 +13,26 @@ namespace App.ViewModels {
         public ObservableCollection<TaskCardViewModel> Tasks { get; }
         
         public bool Removable { get; }
-        public bool IsEditing { get; set; }
+        private bool _isEditing; 
+        public bool IsEditing { 
+            get => _isEditing;
+            set { if (_isEditing != value) {
+                      _isEditing = value;
+                      OnPropertyChanged(nameof(IsEditing));
+                      OnPropertyChanged(nameof(IsNotEditing));
+                  }
+            }
+        }
         public bool IsNotEditing => !IsEditing;
 
+        private string _columnName;
         public string ColumnName { 
-            get => ColumnModel.ColumnName; 
-            set => OnPropertyChanged(nameof(ColumnName));
+            get => _columnName; 
+            set { if (_columnName != value) {
+                      _columnName = value;
+                      OnPropertyChanged(nameof(ColumnName));
+                }
+            }
             // Optional to save the board here if i include a board ref in this vm
         }
 
@@ -26,11 +40,12 @@ namespace App.ViewModels {
         public ICommand StartEditingCommand { get; }
         public ICommand StopEditingCommand { get; }
         public ICommand AddTaskCardCommand { get; }
+        public ICommand CommitColumnNameCommand { get; }
         public ICommand RemoveColumnCommand { get; }
 
         public ColumnViewModel(Column column, bool removable) {
             ColumnModel = column;
-            ColumnName = column.ColumnName;
+            _columnName = column.ColumnName;
             Removable = removable;
 
             Tasks = new(column.Tasks.Select(t => new TaskCardViewModel(t)));
@@ -38,9 +53,13 @@ namespace App.ViewModels {
             StartEditingCommand = new RelayCommand(() => IsEditing = true);
             StopEditingCommand = new RelayCommand(() => IsEditing = false);
             AddTaskCardCommand = new RelayCommand(AddTaskCard);
+            CommitColumnNameCommand = new RelayCommand(() => {
+                    ColumnModel.ColumnName = ColumnName;
+                    IsEditing = false;
+                    });
+
             RemoveColumnCommand = new RelayCommand(() => { 
                     RemoveReq?.Invoke(this); }, () => Removable);
-
 
             AddTaskCard();
         }
@@ -65,12 +84,12 @@ namespace App.ViewModels {
             if (idx > Tasks.Count) idx = Tasks.Count;
 
             if (oldidx == idx || oldidx + 1 == idx) return;
-            
+
             Tasks.Move(oldidx, System.Math.Min(idx, Tasks.Count-1));
             // Optional to save the board here if i include a board ref in this vm
             // JsonDB.SaveBoard(ParentBoard);
         }
-        
+
         /* Removes a given taskcard from the columnlist*/
         public void RemoveTask(TaskCardViewModel card) {
             Tasks.Remove(card);
@@ -93,95 +112,3 @@ namespace App.ViewModels {
         }
     }
 }
-
-// Old version 
-
-/* It wont compile using "using App.Board" So I had to use "App.Board.Board"*/
-// using System.ComponentModel;
-// using System.Text.Json.Serialization;
-// using App.Models;
-// using System.Collections.ObjectModel; 
-//
-// namespace App.Managers {
-//     public class ColumnManager : INotifyPropertyChanged {
-//         public event PropertyChangedEventHandler? PropertyChanged;
-//
-//         [JsonIgnore]
-//         public App.Board.Board ParentBoard { get; set; }
-//         public ObservableCollection<TaskCard> TaskList { get; set; }
-//
-//         // Controls X button visibility
-//         public bool Removable => this != ParentBoard?.FirstColumn;
-//         public bool NameEditable => this != ParentBoard?.FirstColumn;
-//
-//         // Updates name when change in the UI
-//         private string _colName;
-//         public string ColName { get => _colName;
-//             set { if (_colName != value) {
-//                 _colName = value;
-//                 OnPropertyChanged(nameof(ColName));
-//                 }
-//             }
-//         }
-//
-//         // Fields so setting a column to "editing" or not
-//         private bool _isEditing;
-//         public bool IsNotEditing => !_isEditing;
-//         public bool IsEditing { get => _isEditing;
-//             set {
-//                 if (_isEditing != value) {
-//                     _isEditing = value;
-//                     OnPropertyChanged(nameof(IsEditing));
-//                     OnPropertyChanged(nameof(IsNotEditing));
-//                 }
-//             }
-//         }
-//
-//         public ColumnManager() {}
-//
-//         public ColumnManager(App.Board.Board parent) {
-//             ParentBoard = parent;
-//             TaskList = new ObservableCollection<TaskCard>();
-//             _colName = "New Column";
-//         }
-//
-//         public void AddNewTask() {
-//             TaskList.Add(new TaskCard()); 
-//             JsonDB.SaveBoard(ParentBoard);
-//         }
-//
-//         /* For moving a card within its own column*/
-//         public void MoveTask(TaskCard card, int idx) {
-//             if (card == null) return;
-//
-//             int oldidx = TaskList.IndexOf(card);
-//             if (oldidx == -1) return;
-//
-//             if (idx < 0) idx = 0;
-//             if (idx > TaskList.Count) idx = TaskList.Count;
-//
-//             if (oldidx == idx || oldidx + 1 == idx) return;
-//
-//
-//             TaskList.Move(oldidx, System.Math.Min(idx, TaskList.Count-1));
-//             JsonDB.SaveBoard(ParentBoard);
-//         }
-//
-//         /* Removes a given taskcard from the columnlist*/
-//         public void RemoveTask(TaskCard card) {
-//             TaskList.Remove(card);
-//             JsonDB.SaveBoard(ParentBoard);
-//         }
-//
-//         /* Inserts a given taskcard at the given index*/
-//         public void InsertTask(TaskCard card, int idx) {
-//             TaskList.Insert(idx, card); 
-//             JsonDB.SaveBoard(ParentBoard);
-//         }
-//
-//         /* Helper function for updating the UI when a class property changes*/
-//         protected void OnPropertyChanged(string propertyName) {
-//             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-//         }
-//     }
-// }
--- src/Views/ColumnView.axaml.cs
+++ src/Views/ColumnView.axaml.cs
@@ -1,4 +1,7 @@
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using App.ViewModels;
 
 namespace App.Views;
 
@@ -6,7 +9,22 @@ public partial class ColumnView : UserControl {
 
     public ColumnView() {
         InitializeComponent();
-        // DataContext = new BoardViewModel();
     }
 
+    private void EditColumnName(object? sender, PointerPressedEventArgs e) {
+        if (DataContext is ColumnViewModel vm) { 
+            vm.StartEditingCommand.Execute(null);
+            TextBoxName.Focus();
+        }
+    }
+
+    private void EnterKeyHit(object? sender, KeyEventArgs e) {
+        if (e.Key == Key.Enter && DataContext is ColumnViewModel vm)
+            vm.CommitColumnNameCommand.Execute(null);
+    }
+
+    private void NameLostFocus(object? sender, RoutedEventArgs e) {
+        if (DataContext is ColumnViewModel vm)
+            vm.CommitColumnNameCommand.Execute(null);
+    }
 }
